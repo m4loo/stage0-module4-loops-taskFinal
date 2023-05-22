374ed9538452cb60643a7c6e2fccdaab91c341da
m4loo <128167781+m4loo@users.noreply.github.com>
2023-05-23 04:21:01 +0700

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        int spaces = cathetusLength - 1;
+        int matters = 1;
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= spaces; j++) System.out.print(" ");
+            for (int j = 1; j <= matters; j++) System.out.print("*");
+            System.out.println();
+            spaces--;
+            matters++;
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,9 +2,31 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int spaces = cathetusLength - 1;
+        int matters = 1;
+        for (int i = 1; i <= cathetusLength; i++) {
 
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            boolean checker = true;
+            int counter = 0;
+            for (int j = matters; j >= 1 && j <= matters; ) {
+                if (j == 1) checker = false;
+                if (checker) j--;
+                else j++;
+                System.out.print(j);
+                counter++;
+                if (counter >= matters) break;
+            }
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+            matters += 2;
+            spaces -= 2;
+        }
     }
-
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
     }
